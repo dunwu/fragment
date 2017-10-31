e2f410c8e82754115ff607590ce7420d4a33b00a
Zhang Peng <forbreak@163.com>
2017-10-31 23:12:26 +0800

    更新样式文件

--- cnblog/cnblog-diy.js
+++ cnblog/cnblog-diy.js
@@ -1,25 +1,25 @@
 ﻿/*************************************************************************************
- * 评论区 begin
+ * 评论区 BEGIN
  *************************************************************************************/
 //添加 评论区的 形象照
 function addImage() {
-  var spen_html = "<span class='bot' ></span>\
-                         <span class='top'></span>";
-  $(".blog_comment_body").append(spen_html);
+  var spen_html = '<span class=\'bot\' ></span>\
+                         <span class=\'top\'></span>';
+  $('.blog_comment_body').append(spen_html);
 
-  $(".blog_comment_body").before("<div class='body_right' style='float: left;'><a target='_blank'><img  /></a></div>");
-  var feedbackCon = $(".feedbackCon").addClass("clearfix");
+  $('.blog_comment_body').before('<div class=\'body_right\' style=\'float: left;\'><a target=\'_blank\'><img  /></a></div>');
+  var feedbackCon = $('.feedbackCon').addClass('clearfix');
   for (var i = 0; i < feedbackCon.length; i++) {
-    var span = $(feedbackCon[i]).find("span:last")[0].innerHTML || "http://pic.cnitblog.com/face/sample_face.gif";
-    $(feedbackCon[i]).find(".body_right img").attr("src", span);
-    var href = $(feedbackCon[i]).parent().find(".comment_date").next().attr("href");
-    $(feedbackCon[i]).find(".body_right a").attr("href", href);
+    var span = $(feedbackCon[i]).find('span:last')[0].innerHTML || 'http://pic.cnitblog.com/face/sample_face.gif';
+    $(feedbackCon[i]).find('.body_right img').attr('src', span);
+    var href = $(feedbackCon[i]).parent().find('.comment_date').next().attr('href');
+    $(feedbackCon[i]).find('.body_right a').attr('href', href);
 
   }
 }
 //移动一个单位 评论
 function nextRecTop() {
-  var rec = $("#RecentCommentsBlock");
+  var rec = $('#RecentCommentsBlock');
   if (rec.length) {
     var top = rec.scrollTop();
     top++;
@@ -35,7 +35,7 @@ function MobileComment() {
     nextRecTop();
   }, RecCommentTime);
 
-  $("#RecentCommentsBlock").hover(function () {//移进
+  $('#RecentCommentsBlock').hover(function () {//移进
     clearInterval(RecintervalId);
   }, function () {//移出
     RecintervalId = setInterval(function () {
@@ -46,9 +46,9 @@ function MobileComment() {
 
 //添加评论区 背景
 function addtbCommentBody_bg() {
-  var tbcomment = $("#tbCommentBody");
-  if (!tbcomment.hasClass("tbCommentBody_bg")) {
-    tbcomment.addClass("tbCommentBody_bg");
+  var tbcomment = $('#tbCommentBody');
+  if (!tbcomment.hasClass('tbCommentBody_bg')) {
+    tbcomment.addClass('tbCommentBody_bg');
     tbcomment.focus(function () {
       removetbCommentBody_bg();
     });
@@ -56,17 +56,20 @@ function addtbCommentBody_bg() {
 }
 
 function focusoutCommentBody_bg() {
-  $("#tbCommentBody").focusout(function () { addtbCommentBody_bg(); });
+  $('#tbCommentBody').focusout(function () { addtbCommentBody_bg(); });
 }
 //移除评论区 背景
 function removetbCommentBody_bg() {
-  !$(".tbCommentBody_bg").removeClass("tbCommentBody_bg");
-  $("#tbCommentBody").unbind("focus");
+  !$('.tbCommentBody_bg').removeClass('tbCommentBody_bg');
+  $('#tbCommentBody').unbind('focus');
 }
 /*************************************************************************************
- * 评论区 end
+ * 评论区 END
  *************************************************************************************/
 
+/*************************************************************************************
+ * 根据文章中标题自动生成目录 BEGIN
+ *************************************************************************************/
 String.prototype.replaceAll = function (FindText, RepText) {
   regExp = new RegExp(FindText, 'gm');
   return this.replace(regExp, RepText);
@@ -90,8 +93,8 @@ function GenerateContentList() {
 
   var content = '<a name="_labelTop"></a>';
   content += '<div id="navCategory">';
-  content += '<p style="font-size: 18pt; color:#F89448"><b>目录</b></p>';
-  content += '<ul>';
+  content += '<blockquote><p style="font-size: 18pt; color:#A2B4BA"><b>目录</b></p>';
+  content += '<div>';
 
   for (var i = 0; i < nodes.length; i++) {
     var item = '';
@@ -99,18 +102,18 @@ function GenerateContentList() {
     var resolvedTitle = resolveTitle(originTitle);
 
     if (nodes[i].tagName === 'H1') {
-      item = '<li><a style="font-size:18px" href="#' + resolvedTitle + '">' + $(nodes[i]).text() + '</a></li>';
+      item = '<a style="font-size:18px" href="#' + resolvedTitle + '">' + $(nodes[i]).text() + '</a><br>';
     } else if (nodes[i].tagName === 'H2') {
-      item = '<li><a style="font-size:16px" href="#' + resolvedTitle + '">&emsp;&emsp;' + $(nodes[i]).text() + '</a></li>';
+      item = '<a style="font-size:16px" href="#' + resolvedTitle + '">&emsp;&emsp;' + $(nodes[i]).text() + '</a><br>';
     } else if (nodes[i].tagName === 'H3') {
-      item = '<li><a style="font-size:14px" href="#' + resolvedTitle + '">&emsp;&emsp;&emsp;&emsp;' + $(nodes[i]).text() + '</a></li>';
+      item = '<a style="font-size:14px" href="#' + resolvedTitle + '">&emsp;&emsp;&emsp;&emsp;' + $(nodes[i]).text() + '</a><br>';
     } else if (nodes[i].tagName === 'H4') {
-      item = '<li><a style="font-size:12px" href="#' + resolvedTitle + '">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;' + $(nodes[i]).text() + '</a></li>';
+      item = '<a style="font-size:12px" href="#' + resolvedTitle + '">&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;' + $(nodes[i]).text() + '</a><br>';
     }
 
     content += item;
   }
-  content += '</ul>';
+  content += '</blockquote></div>';
   var len = $('#cnblogs_post_body').length;
   if ($('#cnblogs_post_body').length != 0) {
     $($('#cnblogs_post_body')[0]).prepend(content);
@@ -119,6 +122,9 @@ function GenerateContentList() {
   $($('#cnblogs_post_body')[len - 1]).append('<div id=\'signature\'><p>作者：<a href=\'http://www.cnblogs.com/jingmoxukong/\'>静默虚空</a></br>欢迎任何形式的转载，但请务必注明出处。</br>限于本人水平，如果文章和代码有表述不当之处，还请不吝赐教。</p></div>');
 }
 GenerateContentList();
+/*************************************************************************************
+ * 根据文章中标题自动生成目录 END
+ *************************************************************************************/
 
 // 自定义定时器[当元素加载完成是执行回调函数]
 function customTimer(inpId, fn) {
@@ -151,8 +157,8 @@ $(function () {
 
   //添加 评论区的 形象照
   MobileComment();//移动评论
-  customTimer(".blog_comment_body", addImage);
-  customTimer("#tbCommentBody", function () {
+  customTimer('.blog_comment_body', addImage);
+  customTimer('#tbCommentBody', function () {
     addtbCommentBody_bg();
     focusoutCommentBody_bg();
   });
